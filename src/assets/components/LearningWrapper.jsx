026e7ada0c801cd0d01aa120670063b137f57b71
Izabela Jabłecka <izjab@icloud.com>
2023-11-09 22:13:45 +0100

    flashcard answer buttons and animation for multiple cards

--- src/assets/components/LearningWrapper.jsx
+++ src/assets/components/LearningWrapper.jsx
@@ -1,45 +1,36 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getData, getDeckAndFlashcards } from "../../api/api.js";
-import Flashcard from "./flashcards/Flashcard.jsx";
+import { getDeckAndFlashcards } from "/src/api/api.js";
+import FlashCards from "./flashcards/Flashcards.jsx";
 
 function LearningWrapper() {
   const { deckId } = useParams();
   const [deck, setDeck] = useState(null);
   const [flashcards, setFlashcards] = useState([]);
-
   useEffect(() => {
     const controller = new AbortController();
-
     getDeckAndFlashcards(deckId, controller.signal).then(
       ([deckResponse, flashcardsResponse]) => {
         setDeck(deckResponse);
         setFlashcards(flashcardsResponse);
       },
     );
-
     return () => {
       controller.abort();
     };
   }, []);
-
   if (!deck) {
     return <div>Loading...</div>;
   }
-
   return (
     <div>
       Learning: {deckId} {deck.name} {deck.description}
       {flashcards.length ? (
-        <Flashcard
-          question={flashcards[0].question}
-          answer={flashcards[0].answer}
-        />
+        <FlashCards flashcards={flashcards} />
       ) : (
         <p>Flashcards not found</p>
       )}
     </div>
   );
 }
-
 export default LearningWrapper;
